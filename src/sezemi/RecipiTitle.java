05f006f1325bd305f36ad9b933264921efa26ae1
Shintaro Watanabe <mashinta1226@gmail.com>
2015-06-06 11:43:33 +0900

    spec3: 別ファイルからタイトルを読み込む

--- src/sezemi/RecipiTitle.java
+++ src/sezemi/RecipiTitle.java
@@ -1,7 +1,31 @@
 package sezemi;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class RecipiTitle {
-	public static void main(String args[]){
-		System.out.println("オムライス");
+	public static void main(String args[]) throws FileNotFoundException{
+		try {
+			// 読み取りファイルの指定
+			File target_file = new File("recipi_title.txt");
+			FileReader filereader = new FileReader(target_file);
+			
+			// 書き出す文字が残っているかの判定
+			int exist_title = filereader.read();
+			
+			while(exist_title != -1){
+				// 一文字ずつ書き出す
+				System.out.print((char)exist_title);
+				
+				exist_title = filereader.read();
+			}
+			
+			filereader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
