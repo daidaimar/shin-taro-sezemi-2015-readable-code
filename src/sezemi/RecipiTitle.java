df35ca06059b4503c9cb2ac71ba185d04a4aab9e
Shintaro Watanabe <mashinta1226@gmail.com>
2015-06-06 11:56:19 +0900

    spec4: レシピタイトルの読み取り方を変更

--- src/sezemi/RecipiTitle.java
+++ src/sezemi/RecipiTitle.java
@@ -1,5 +1,6 @@
 package sezemi;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -10,21 +11,21 @@ public class RecipiTitle {
 		try {
 			// 読み取りファイルの指定
 			File target_file = new File("recipi_title.txt");
-			FileReader filereader = new FileReader(target_file);
+			BufferedReader br = new BufferedReader(new FileReader(target_file));
 			
-			// 書き出す文字が残っているかの判定
-			int exist_title = filereader.read();
+			// タイトルを一行ずつ読み取る
+			String recipi_title = br.readLine();
 			
-			while(exist_title != -1){
-				// 一文字ずつ書き出す
-				System.out.print((char)exist_title);
-				
-				exist_title = filereader.read();
+			while(recipi_title != null){
+				// 一行ずつ書き出す
+				System.out.println(recipi_title);
+				// 次のタイトルを取得
+				recipi_title = br.readLine();
 			}
-			
-			filereader.close();
+			br.close();
+		} catch (FileNotFoundException e){
+			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
